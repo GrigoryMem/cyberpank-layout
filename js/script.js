37c83a0fa902927a826f080ac569d661a82306fa
Grogory Morozov <morozehc@yandex.ru>
2023-09-29 09:14:35 +0300

    3. intro full

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const intro = document.querySelector(".intro");
+
+console.log(intro)
+
+let hero1 = "intro__theme_1";
+let hero2 = "intro__theme_2";
+let hero3 = "intro__theme_3";
+
+
+
+// setInterval(()=>{
+
+//     if(intro.classList.contains(hero1)){
+//         intro.classList.toggle(hero2);
+//     }
+//     if(intro.classList.contains(hero2)){
+//         intro.classList.toggle(hero3);
+
+//     }
+//     if(intro.classList.contains(hero3)){
+//         intro.classList.toggle(hero1);
+
+//     }
+
+    
+    
+   
+// },2000)
